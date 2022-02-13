7bfee86e560181da87316610f7f6a64eaffc5ade
Patrick Vilchez <pvilchez794@gmail.com>
2022-02-13 00:26:10 -0500

    Users list and disconnect socket added

--- server.js
+++ server.js
@@ -9,35 +9,52 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
-// app.use(cors({ origin: process.env.HEROKU_ORIGIN }))
 app.use(cors())
 
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    // origin: ['https://chat-room-client-psi.vercel.app/', 'https://chat-room-client-patrick564.vercel.app/', 'https://chat-room-client-git-main-patrick564.vercel.app/'],
-    // origin: 'https://chat-room-client-*.vercel.app',
     origin: process.env.ORIGIN,
     // credentials: true,
   }
 })
 
 const crypto = require('crypto')
+let users = new Map()
 
 server.listen(PORT, () => {
   console.log(`Server at ${PORT}`)
 })
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   const uuid = crypto.randomUUID()
 
-  socket.on('user-connected', (username) => {
-    let userData = { username, uuid }
+  socket.on('join-room', (room) => {
+    socket.join(room)
+  })
 
-    socket.emit('user-info', userData)
+  socket.on('user-connected', (username) => {
+    users.set(socket.id, { id: uuid, username })
+    socket.emit('user-info', users.get(socket.id))
+    io.emit('user-info-public', Array.from(users.values()))
   })
 
   socket.on('send-message', (msg) => {
-    io.emit('message', msg)
+    let userSender = users.get(socket.id)
+
+    io.to('general').emit('message', { msg, user: userSender })
+  })
+
+  socket.on('disconnect', () => {
+    let deletedUser = users.get(socket.id)
+
+    users.delete(socket.id)
+    // io.emit('logout', {
+    //   deletedUser,
+    //   usersList: Array.from(users.values())
+    // })
+    io.emit('logout', deletedUser)
+    io.emit('user-info-public', Array.from(users.values()))
+    socket.leave('general')
   })
 })
